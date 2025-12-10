17cc9a2887acdae82b0de5668c95258e799e8d65
adahmen <albert.dahmen@gmail.com>
2025-12-10 18:19:16 +0000

    Füge Konstanten für die Xenia-Komponente hinzu

---
+++ custom_component/xenia/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "xenia"
+
+CONF_IP_ADDRESS = "ip_address"
+CONF_SCAN_INTERVAL_OVERVIEW = "scan_interval_overview"
+CONF_SCAN_INTERVAL_SINGLE = "scan_interval_single"
+
+DEFAULT_SCAN_INTERVAL_OVERVIEW = 10
+DEFAULT_SCAN_INTERVAL_SINGLE = 60
+
+API_OVERVIEW = "/api/v2/overview"
+API_OVERVIEW_SINGLE = "/api/v2/overview_single"
+API_MACHINE_CONTROL = "/api/v2/machine/control"
